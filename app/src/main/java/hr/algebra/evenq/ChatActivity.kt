6e2bd199b7d7cb3e59c5706c8aba716ee9697b9c
Josip Kelentrić <jkelent@racunarstvo.hr>
2022-05-05 10:44:09 +0200

    Bug fixes

--- app/src/main/java/hr/algebra/evenq/ChatActivity.kt
+++ app/src/main/java/hr/algebra/evenq/ChatActivity.kt
@@ -1,26 +1,24 @@
 package hr.algebra.evenq
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import hr.algebra.evenq.adapters.EXTRA_EVENT_DATE
 import hr.algebra.evenq.adapters.EXTRA_EVENT_NAME
 import hr.algebra.evenq.databinding.ActivityChatBinding
 
-class ChatActivity: AppCompatActivity() {
+class ChatActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityChatBinding
 
     private lateinit var eventName: String
     private lateinit var eventDate: String
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         eventName = intent.getStringExtra(EXTRA_EVENT_NAME).toString()
         eventDate = intent.getStringExtra(EXTRA_EVENT_DATE).toString()
     }
-
 }
